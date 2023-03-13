25904348b81ed0663aa79fc45696c2d9ee9f83a2
jericosanvictores <jericosanvictores@gmail.com>
2023-03-13 17:30:55 +0800

    Replaced the initial route to logIn

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.grey,
       ),
-      initialRoute: verifyCode,
+      initialRoute: logIn,
       getPages: AppPages.pages,
     );
   }
