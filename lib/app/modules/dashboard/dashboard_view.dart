7b4ac1facbb7ad0ac938dea3086d65d7a8ea11d2
jericosanvictores <jericosanvictores@gmail.com>
2023-03-14 10:44:18 +0800

    Added dashboard view, controller, as well as routes

---
+++ lib/app/modules/dashboard/dashboard_view.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class DashboardView extends StatefulWidget {
+  const DashboardView({super.key});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _DashboardViewState createState() => _DashboardViewState();
+}
+
+class _DashboardViewState extends State<DashboardView> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- lib/app/routes/route_pages.dart
+++ lib/app/routes/route_pages.dart
@@ -1,4 +1,5 @@
 import 'package:get/get.dart';
+import 'package:pennywise_app/app/modules/dashboard/dashboard_view.dart';
 import 'package:pennywise_app/app/modules/login/login_view.dart';
 import 'package:pennywise_app/app/modules/sign_up/sign_up_view.dart';
 import 'package:pennywise_app/app/routes/route_names.dart';
@@ -18,6 +19,10 @@ class AppPages {
     GetPage(
       name: verifyCode,
       page: () => const VerifyView(),
+    ),
+    GetPage(
+      name: dashBoard,
+      page: () => const DashboardView(),
     ),
   ];
 }
