44abd98a0814c1629344618ce307cd55fc57bcf1
jericosanvictores <jericosanvictores@gmail.com>
2023-03-14 10:25:16 +0800

    Fixed validation error on login

--- lib/app/modules/login/login_view.dart
+++ lib/app/modules/login/login_view.dart
@@ -20,71 +20,82 @@ class _LoginViewState extends State<LoginView> {
   final phoneNumberController = TextEditingController();
   final passwordController = TextEditingController();
 
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.only(left: 20, right: 20),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            const LogoContainer(),
-            SizedBox(height: 20),
-            const AppHeaderText(
-              text: 'sign in to',
-            ),
-            const AppHeaderText(
-              text: 'pennywise.',
-              style: kBoldHeaderStyle,
-            ),
-            SizedBox(height: 20),
-            RedBox(),
-            SizedBox(height: 60),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                AppTextFormField(
-                  hint: 'Phone number...',
-                  icon: Icons.phone_android_rounded,
-                  controller: phoneNumberController,
-                ),
-                SizedBox(height: 20),
-                AppTextFormField(
-                  hint: 'Password...',
-                  icon: Icons.lock_rounded,
-                  controller: passwordController,
-                  obscureText: true,
-                ),
-                SizedBox(height: 20),
-                AppFilledButton(
-                  text: 'Sign In',
-                  color: tertiaryColor,
-                  onPressed: () {
-                    //placeholder
-                    debugPrint("Login");
-                  },
-                ),
-                SizedBox(height: 60),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const AppRegularText(
-                      text: 'Don\'t have an account?',
-                      color: tertiaryColor,
-                    ),
-                    AppRegularText(
-                      text: ' Register.',
-                      color: tertiaryColor,
-                      onTap: () {
-                        //placeholder
-                        debugPrint("Register");
-                      },
-                      style: kBoldTextStyle,
-                    ),
-                  ],
-                ),
-              ],
+            Form(
+              key: _formKey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const LogoContainer(),
+                  SizedBox(height: 20),
+                  const AppHeaderText(
+                    text: 'sign in to',
+                  ),
+                  const AppHeaderText(
+                    text: 'pennywise.',
+                    style: kBoldHeaderStyle,
+                  ),
+                  SizedBox(height: 20),
+                  RedBox(),
+                  SizedBox(height: 60),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      AppTextFormField(
+                        validator: (value) => null,
+                        hint: 'Phone number...',
+                        icon: Icons.phone_android_rounded,
+                        controller: phoneNumberController,
+                      ),
+                      SizedBox(height: 20),
+                      AppTextFormField(
+                        validator: (value) => null,
+                        hint: 'Password...',
+                        icon: Icons.lock_rounded,
+                        controller: passwordController,
+                        obscureText: true,
+                      ),
+                      SizedBox(height: 20),
+                      AppFilledButton(
+                        text: 'Sign In',
+                        color: tertiaryColor,
+                        onPressed: () {
+                          //placeholder
+                          debugPrint("Login");
+                        },
+                      ),
+                      SizedBox(height: 60),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const AppRegularText(
+                            text: 'Don\'t have an account?',
+                            color: tertiaryColor,
+                          ),
+                          AppRegularText(
+                            text: ' Register.',
+                            color: tertiaryColor,
+                            onTap: () {
+                              //placeholder
+                              debugPrint("Register");
+                            },
+                            style: kBoldTextStyle,
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ],
         ),
