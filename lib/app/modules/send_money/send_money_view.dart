9396d172242251ad40677210c0428473cb7f5736
jericosanvictores <jericosanvictores@gmail.com>
2023-03-14 17:44:49 +0800

    Added routes, view, controllers for sendmoney

---
+++ lib/app/modules/send_money/send_money_view.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class SendMoneyView extends StatefulWidget {
+  const SendMoneyView({Key? key}) : super(key: key);
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _SendMoneyViewState createState() => _SendMoneyViewState();
+}
+
+class _SendMoneyViewState extends State<SendMoneyView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
--- lib/app/routes/route_names.dart
+++ lib/app/routes/route_names.dart
@@ -2,3 +2,4 @@ const String signUp = '/signUp';
 const String logIn = '/logIn';
 const String verifyCode = '/verifyCode';
 const String dashBoard = '/dashBoard';
+const String sendMoney = '/sendMoney';
