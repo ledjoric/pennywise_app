b48a44c747bb40a8a288a8cff80f0b77936b4648
@ledjoric <ledjoricvermont@gmail.com>
2023-03-16 10:29:01 +0800

    added dashoboard controller

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.grey,
       ),
-      initialRoute: logIn,
+      initialRoute: dashBoard,
       getPages: AppPages.pages,
     );
   }
