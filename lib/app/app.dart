ff71729aa8cea773b7e0a09fd43041dae3fcee32
@ledjoric <ledjoricvermont@gmail.com>
2023-03-21 12:44:04 +0800

    changed initial route

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
         colorSchemeSeed: tertiaryColor,
       ),
-      initialRoute: verifyCode,
+      initialRoute: logIn,
       getPages: AppPages.pages,
     );
   }
